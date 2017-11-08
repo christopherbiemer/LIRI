80195437c54fc0af81030c9474f66d79c66ad6bd
christopherbiemer <31355994+christopherbiemer@users.noreply.github.com>
2017-11-08 14:41:56 -0600

    added log text file to track output

--- liri.js
+++ liri.js
@@ -22,8 +22,15 @@ var spotify_call = new spotify({
 var command = process.argv[2];
 var specific = process.argv[3];
 
+var morespecific;
+
+if (specific == undefined) {
+	morespecific = "";
+}
+
 function master() {
-	
+	fs.appendFile("log.txt", command + " " + morespecific, function(error) {
+				})
 	if (command == "my-tweets") {
 		var params = {screen_name: 'beligerentitis'};
 		twitter_call.get('statuses/user_timeline', params, function(error, tweets, response) {
@@ -31,11 +38,17 @@ function master() {
 				console.log(" ");
 				for (var i = 19; i>-1; i--) {
 					console.log('"' + tweets[i]["text"] + '"');
+					fs.appendFile("log.txt", ", " + tweets[i]["text"], function(error) {
+					})
 					var timestamp = tweets[i]["created_at"];
 					timestamp = moment(timestamp).format("LLLL");
 					console.log("("+ timestamp + ")");
-					console.log(" ");
+					fs.appendFile("log.txt", "-->" + timestamp, function(error) {
+					})
+					console.log(" ");		
 				}
+			fs.appendFile("log.txt", " || ", function(error) {
+					})
 			}
 		});
 	}
@@ -48,9 +61,17 @@ function master() {
 			.then(function(data) {
 				console.log("");
 				console.log("Artist(s): " + data['artists'][0]['name']);
+				fs.appendFile("log.txt", ", " + data['artists'][0]['name'], function(error) {
+					})
 				console.log("Track: " + data['name']);
+				fs.appendFile("log.txt", ", " + data['name'], function(error) {
+					})
 				console.log("Preview URL: " + data['preview_url']);
+				fs.appendFile("log.txt", ", " + data['preview_url'], function(error) {
+					})
 				console.log("Album: " + data['album']['name']);
+				fs.appendFile("log.txt", ", " + data['album']['name'] + " || ", function(error) {
+					})
 				console.log("");
 			})
 			.catch(function(err) {
@@ -65,10 +86,18 @@ function master() {
 				}
 				console.log("");
 				console.log("Artist(s): " + data['tracks']['items'][0]['artists'][0]['name']);
+				fs.appendFile("log.txt", ", " + data['tracks']['items'][0]['artists'][0]['name'], function(error) {
+					})
 				console.log("Track: " + data['tracks']['items'][0]['name']);
+				fs.appendFile("log.txt", ", " + data['tracks']['items'][0]['name'], function(error) {
+					})
 				console.log("Preview URL: " + data['tracks']['items'][0]['preview_url']);
+				fs.appendFile("log.txt", ", " + data['tracks']['items'][0]['preview_url'], function(error) {
+					})
 				console.log("Album: " + data['tracks']['items'][0]['album']['name']);
-				console.log("");
+				fs.appendFile("log.txt", ", " + data['tracks']['items'][0]['album']['name'] + " || ", function(error) {
+					})
+				console.log("");			
 			})
 		}
 	}
@@ -88,13 +117,29 @@ function master() {
 				var rotten_tomatoes = JSON.parse(body).Ratings[1]["Value"];
 				console.log("");
 				console.log("Title: " + JSON.parse(body).Title);
+				fs.appendFile("log.txt", ", " + JSON.parse(body).Title, function(error) {
+					})
 				console.log("Year: " + JSON.parse(body).Year);
+				fs.appendFile("log.txt", ", " + JSON.parse(body).Year, function(error) {
+					})
 				console.log("IMDB Score: " + JSON.parse(body).imdbRating);
+				fs.appendFile("log.txt", ", " + JSON.parse(body).imdbRating, function(error) {
+					})
 				console.log("Rotten Tomatoes: " + rotten_tomatoes);
+				fs.appendFile("log.txt", ", " + rotten_tomatoes, function(error) {
+					})
 				console.log("Country: " + JSON.parse(body).Country);
+				fs.appendFile("log.txt", ", " + JSON.parse(body).Country, function(error) {
+					})
 				console.log("Language: " + JSON.parse(body).Language);
+				fs.appendFile("log.txt", ", " + JSON.parse(body).Language, function(error) {
+					})
 				console.log("Plot: " + JSON.parse(body).Plot);
+				fs.appendFile("log.txt", ", " + JSON.parse(body).Plot, function(error) {
+					})
 				console.log("Actors: " + JSON.parse(body).Actors);
+				fs.appendFile("log.txt", ", " + JSON.parse(body).Actors + " || ", function(error) {
+					})
 				console.log("");
 			}
 		})
